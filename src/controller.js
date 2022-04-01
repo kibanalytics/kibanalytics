45c4edcf737301de355b74eb038890f5d5fb3ccd
ibrahim-netto <ibrahim.netto@outlook.com>
2022-04-01 18:51:10 -0300

    - Boolean casting for EXPRESS_CORS and VALIDATE_JSON_SCHEMA env variables - User agent parser - Platform property added to tracker payload - Session cookie options - Session page views counter - Session ts for pageview event - New schema for pushed data

--- src/controller.js
+++ src/controller.js
@@ -1,12 +1,14 @@
 const { v4: uuidv4 } = require('uuid');
 const redisClient = require('./redis-client');
 const validator = require('./validator');
-const { getServerMetric } = require('./utils');
+const uaParser = require('ua-parser-js');
+const getServerMetric = require('./server-metrics');
 
 module.exports.collect = async (req, res, next) => {
     try {
-        if (process.env.VALIDATE_JSON_SCHEMA?.toLowerCase() === 'true') {
-            const eventType = req.body.type;
+        const eventType = req.body.type;
+
+        if (!!+process.env.VALIDATE_JSON_SCHEMA) {
             const validate = validator.getSchema(eventType);
 
             if (!validate(req.body.payload)) {
@@ -15,17 +17,32 @@ module.exports.collect = async (req, res, next) => {
             }
         }
 
-        const serverMetrics = getServerMetric(req);
+        if (eventType === 'pageview') {
+            (req.session.views) ? req.session.views++ : req.session.views = 1;
+        }
+
         const data = {
-            uuid: uuidv4(),
-            eventType: req.body.type,
-            ...req.body.payload,
-            ... serverMetrics
+            event: {
+                _id: uuidv4(),
+                type: req.body.type,
+                payload: req.body.payload
+            },
+            session: {
+                _id: req.session.id,
+                views: req.session.views,
+                previousUrl: req.session.previousUrl
+            },
+            userAgent: uaParser(req.headers['user-agent']),
+            metrics: getServerMetric(req)
         };
 
-        // @TODO rPush nested objects
+        if (eventType === 'pageview') {
+            req.session.ts = new Date();
+            req.session.previousUrl = req.body.payload.url;
+        }
+
         await redisClient.rPush(process.env.TRACKING_KEY, JSON.stringify(data));
-        res.json({ status: 'success', uuid: data.uuid });
+        res.json({ status: 'success', eventId: data.eventId });
     } catch (err) {
         next(err);
     }
