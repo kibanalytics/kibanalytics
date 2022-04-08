799f8cc7771a7517f20e5358dd34c24366d006c4
ibrahim-netto <ibrahim.netto@outlook.com>
2022-04-08 11:43:00 -0300

    - CSP inline event handler bug-fix - tracker serverUrl defaults to 'location.origin' - Added compression middleware for use on servers without reverse-proxy - Optional compression with enviroment variable EXPRESS_GZIP

--- public/index.js
+++ public/index.js
@@ -1,12 +1,19 @@
 kbs.serverSideData = { foo: 'bar' };
+console.log('serverSideData', kbs.serverSideData);
+
 kbs.callback = (response) => {
     console.log('callback', response);
 }
-console.log('serverSideData', kbs.serverSideData);
 
 document.addEventListener('DOMContentLoaded', () => {
-    const buttonEl = document.getElementById('programmaticallyCall');
-    buttonEl.addEventListener('click', async () => {
+    const buttonEl01 = document.getElementById('button01');
+    buttonEl01.addEventListener('click', () => console.log('Click Event'));
+
+    const buttonEl02 = document.getElementById('button02');
+    buttonEl02.addEventListener('click', () => console.log('Click Event (Custom Data)'));
+
+    const buttonEl03 = document.getElementById('button03');
+    buttonEl03.addEventListener('click', async () => {
         const { kbs } = window;
 
         const data = {
