81e676a529545fd318fa79e742132e214d2c4b76
ibrahim-netto <ibrahim.netto@outlook.com>
2022-04-08 13:07:15 -0300

    - Optional node cluster

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 
+const os = require('os')
+const cluster = require('cluster')
 const logger = require('./src/logger');
 const redisClient = require('./src/redis-client');
 const express = require('express');
@@ -15,7 +17,7 @@ const session = require('./src/session');
 const controller = require('./src/controller');
 const errorHandler = require('./src/error-handler');
 
-(async () => {
+const init = async () => {
     await redisClient.connect();
     const app = express();
 
@@ -67,6 +69,24 @@ const errorHandler = require('./src/error-handler');
     app.use(Sentry.Handlers.errorHandler());
     app.use(errorHandler);
     app.listen(process.env.EXPRESS_PORT, () => {
-        logger.info(`Express app listening on port ${process.env.EXPRESS_PORT}`)
+        logger.info(`Express app listening on port ${process.env.EXPRESS_PORT}`);
     });
-})();
+};
+
+const clusterWorkerSize = (!!+process.env.NODE_CLUSTER) ? os.cpus().length : 1;
+
+if (clusterWorkerSize > 1) {
+    if (cluster.isMaster) {
+        for (let i = 0; i < clusterWorkerSize; i++) {
+            cluster.fork();
+        }
+
+        cluster.on('exit', (worker) => {
+            logger.info(`Worker ${worker.id} has exited`);
+        });
+    } else {
+        init();
+    }
+} else {
+    init();
+}
