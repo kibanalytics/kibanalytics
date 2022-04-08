8c7b932403f3b3ec949f7646fade25203a2acfda
ibrahim-netto <ibrahim.netto@outlook.com>
2022-04-08 13:07:15 -0300

    - Optional node cluster

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,22 @@
-const { createLogger, format: { combine, colorize, simple }, transports } = require('winston');
+const { createLogger, format, transports } = require('winston');
+const { combine, colorize, splat, timestamp, printf } = format;
+
+const custom = printf(({ level, message, timestamp, ...metadata }) => {
+    let msg = `${timestamp} \x1b[34m${process.pid}\x1b[0m ${level}: ${message} `;
+
+    if (Object.keys(metadata).length > 0) {
+        msg += JSON.stringify(metadata)
+    }
+
+    return msg
+});
 
 const logger = createLogger({
     format: combine(
         colorize(),
-        simple()
+        splat(),
+        timestamp(),
+        custom
     ),
     transports: [new transports.Console()]
 });
