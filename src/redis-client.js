ca71388b9e1bd08b3a9fad1f59adb29d9e71a942
ibrahim-netto <ibrahim.netto@outlook.com>
2022-04-22 14:40:12 -0300

    - Redis connection with REDIS_URI. - Enviroment variables documentation.

--- src/redis-client.js
+++ src/redis-client.js
@@ -2,12 +2,7 @@ const { createClient } = require('redis');
 const logger = require('./logger');
 
 const options = {
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-    },
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
+    url: process.env.REDIS_URI,
     legacyMode: true // Required for connect-redis
 }
 const client = createClient(options);
