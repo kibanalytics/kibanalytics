156b208b9610f0b040c7cef48e0552ad351a3f4c
ibrahim-netto <ibrahim.netto@outlook.com>
2022-04-29 07:29:08 -0300

    - cookie sameSite = 'lax'

--- src/session.js
+++ src/session.js
@@ -15,7 +15,7 @@ module.exports = session({
             Cookies will be sent in all contexts, i.e. in responses to both first-party and cross-origin requests.
             If SameSite=None is set, the cookie Secure attribute must also be set (or the cookie will be blocked).
         */
-        sameSite: (process.env.NODE_ENV === 'production') ? 'none' : false,
+        sameSite: (process.env.NODE_ENV === 'production') ? 'lax' : false,
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         maxAge: parseInt((process.env.EXPRESS_COOKIE_TTL || 7776000) * 1000) // defaulted to 90d
