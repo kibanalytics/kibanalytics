a7f0c24c804bc4af8a49a272d02b0cfbcc5bfdea
ibrahim-netto <ibrahim.netto@outlook.com>
2022-11-16 09:01:26 -0300

    CORS bug-fix

--- src/cors-options.js
+++ src/cors-options.js
@@ -2,9 +2,6 @@ const allowedOrigins = process.env.EXPRESS_ALLOWED_ORIGINS
     .split(',')
     .map(v => new RegExp(v));
 
-if (allowedOrigins.find(origin => origin === '*'))
-    throw new Error(`Invalid origin '*'. Credentials not supported if the CORS header 'Access-Control-Allow-Origin' is '*'.`);
-
 module.exports = {
     origin: allowedOrigins,
     credentials: true, // Enable HTTP cookies over CORS
